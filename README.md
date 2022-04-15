45aa92d16c690f4fe3484a6b1f97c2d53a15f78e
mufidfarhan <60894094+mufidfarhan@users.noreply.github.com>
2022-04-15 23:38:22 +0700

    jquery

--- README.md
+++
@@ -1,2 +0,0 @@
-# 1402020027-MufidFarhanMuhana
-Repositori ini akan digunakan untuk seluruh kegiatan mata kuliah desain dan pemrograman web
---
+++ script/script.js
@@ -0,0 +1,75 @@
+$(document).ready(function () {
+	$("#paket-data").click(function () {
+		$(".content-nav").removeClass("active");
+		$(this).addClass("active");
+		$("#nom-pulsa").hide();
+		$("#nom-data").show();
+		$("#no-telp").val("");
+		$("#nom-pulsa, #nom-data, #button-beli").prop("disabled", true);
+		$("#nom-data").val("");
+	});
+
+	$("#pulsa").click(function () {
+		$(".content-nav").removeClass("active");
+		$(this).addClass("active");
+		$("#nom-pulsa").show();
+		$("#nom-data").hide();
+		$("#no-telp").val("");
+		$("#nom-pulsa, #nom-data, #button-beli").prop("disabled", true);
+		$("#nom-pulsa").val("");
+	});
+
+	$("#no-telp").keyup(function () {
+		var inputVal = $(this).val();
+		var numericReg = /^(^\+62|62|^08)(\d{3,4}-?){2}\d{3,4}$/;
+		if (numericReg.test(inputVal)) {
+			$("#nom-pulsa, #nom-data").prop("disabled", false);
+		} else {
+			$("#nom-pulsa, #nom-data").prop("disabled", true);
+		}
+	});
+
+	$("select option[value='']:selected").attr("disabled", "disabled");
+
+	$("#nom-pulsa, #nom-data").change(function () {
+		$("#button-beli").prop("disabled", false);
+	});
+
+	$("#button-beli").click(function () {
+		if ($("#pulsa").hasClass("active")) {
+			if (
+				confirm(
+					"Pembelian pulsa sebesar " +
+						$("#nom-pulsa").val().split(",")[0] +
+						" ke nomor " +
+						$("#no-telp").val()
+				)
+			) {
+				alert(
+					"Total harga pulsa Rp. " +
+						$("#nom-pulsa").val().split(",")[1] +
+						",- ke nomor " +
+						$("#no-telp").val() +
+						" sedang di proses. Terima Kasih :)"
+				);
+			}
+		} else if ($("#paket-data").hasClass("active")) {
+			if (
+				confirm(
+					"Pembelian paket data " +
+						$("#nom-data").val().split(",")[0] +
+						" ke nomor " +
+						$("#no-telp").val()
+				)
+			) {
+				alert(
+					"Total harga paket data Rp. " +
+						$("#nom-data").val().split(",")[1] +
+						",- ke nomor " +
+						$("#no-telp").val() +
+						" sedang di proses. Terima Kasih :)"
+				);
+			}
+		}
+	});
+});
